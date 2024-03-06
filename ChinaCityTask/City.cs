1a2069806832ba03712f7fe00142da9e4e178aa7
AykhanTa <aykhanft@code.edu.az>
2024-03-06 17:04:24 +0400

    Add project files.

---
+++ ChinaCityTask/City.cs
@@ -0,0 +1,61 @@
+﻿namespace ChinaCityTask
+{
+     class City
+     {
+        string _name;
+        public string Name {
+            get 
+            {
+                return _name;
+            }
+            set
+            {
+                bool isLetter = true;
+                if (!(value.Length > 3 && value.Length < 20))
+                {
+                    Console.WriteLine("serte uygun deyil");
+                    return;
+                }
+               
+                foreach (var item in value)
+                {
+                    
+                    if (!(char.IsLetter(item)))
+                    {
+                        isLetter= false;
+                        break;
+                    }
+                }
+                if (isLetter)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Serte uygun deyilllll");
+                    return;
+                }
+                
+                
+            }
+        }
+        int _population;
+        public int Population {
+            get
+            {
+                return _population;
+            }
+            set 
+            {
+                if (value<0)
+                {
+                    Console.WriteLine("Deyer 0-dan asagi ola bilmez.");
+                    return;
+                }
+                _population= value;
+            } 
+        }
+
+        
+    }
+}
---
+++ ChinaCityTask/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ChinaCityTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City city = new City();
+            city.Name = "Nakhcivan";
+            city.Population = 100000;
+            Console.WriteLine(city.Name);
+            Console.WriteLine(city.Population);
+        }
+    }
+}
